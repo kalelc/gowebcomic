0700102f20e738ffe2c805f0568191966f292fbc
Andres Colonia <andreskal@gmail.com>
2018-12-11 19:12:07 -0300

    create basic comic model to save response from api

---
+++ comic.go
@@ -0,0 +1,18 @@
+package main
+
+const ServerURL = "https://xkcd.com"
+const Limit = 2000
+
+type Comic struct {
+	Month      string `json:"month"`
+	Num        int    `json:"num"`
+	Link       string `json:"link"`
+	Year       string `json:"year"`
+	News       string `json:"news"`
+	SafeTittle string `json:"safe_title"`
+	Transcript string `json:"transcript"`
+	Alt        string `json:"alt"`
+	Img        string `json:"img"`
+	Title      string `json:"title"`
+	Day        string `json:"day"`
+}
