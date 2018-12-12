72d59c37ac9b0d3c761f098d81d1e4709ed8892b
Andres Colonia <andreskal@gmail.com>
2018-12-12 11:50:47 -0300

    create function to search file in data

--- file_handler.go
+++ file_handler.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"os"
 	"strconv"
 )
 
 const ComicPATH = "data/"
+
+func SearchFile(name string) (status bool) {
+	files, err := ioutil.ReadDir("data/")
+
+	HandlerError(err)
+
+	for _, file := range files {
+		if name == file.Name() {
+			status = true
+			break
+		}
+	}
+	return
+}
 
 func SaveFile(comic *Comic) {
 	data, err := json.Marshal(comic)
