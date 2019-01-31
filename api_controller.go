82fbf487d9f2f804764efd97d63f5b42accb6beb
Andres Colonia <andreskal@gmail.com>
2019-01-31 18:43:01 -0300

    authorize with jwt

--- api_controller.go
+++ api_controller.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"strconv"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 )
 
 func Show(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-
 	id := vars["id"]
+
+	queryValues := r.URL.Query()
+	tokenString := queryValues["jwt"][0]
 
 	var status bool
 	var comic *Comic
@@ -27,10 +31,17 @@ func Show(w http.ResponseWriter, r *http.Request) {
 		HandlerError(err)
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	payload, err := json.Marshal(comic)
+	fmt.Println(tokenString)
 
-	HandlerError(err)
+	token, err := Auth(tokenString)
 
-	w.Write(payload)
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		w.Header().Set("Content-Type", "application/json")
+		payload, err := json.Marshal(comic)
+		fmt.Println(claims)
+		w.Write(payload)
+	} else {
+		fmt.Println(err)
+	}
+
 }
