2ba07ca9391c8bf7340151510239aa7847f0330e
Andres Colonia <andreskal@gmail.com>
2019-01-31 16:02:14 -0300

    include server

--- main.go
+++ main.go
@@ -1,33 +1,13 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"os"
-	"strconv"
+	"fmt"
+	"net/http"
 )
 
-var number = flag.Int("number", 1, "Is the number of commic to downloads")
-
 func main() {
-	flag.Parse()
-
-	num := *number
-
-	var status bool
-	var comic *Comic
-	var err error
-
-	comic, status = SearchFile(strconv.Itoa(num))
-
-	HandlerError(err)
-
-	if !status {
-		comic, err = Get(num)
-		HandlerError(err)
-	}
-
-	if err := templ.Execute(os.Stdout, comic); err != nil {
-		log.Fatal(err)
-	}
+	router := NewRouter()
+	fmt.Println("* Listening on 8000 port")
+	fmt.Println("* Use Ctrl-C to stop")
+	http.ListenAndServe(":8000", router)
 }
